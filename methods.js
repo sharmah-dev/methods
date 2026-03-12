46328fc9cd0f6c0f1e9bb293276737b384b15f80
sharmah-dev <rodgersirma521@gmail.com>
2026-03-12 22:32:27 +0300

    first commit

---
+++ methods.js
@@ -0,0 +1,56 @@
+//Q1
+// creating a variable
+let sentence = "I love coding"
+
+// printing
+console.log(sentence)
+
+//Q2
+//number of characters
+console.log(sentence.length)
+
+// to uppercase
+console.log(sentence.toUpperCase())
+
+//to lowercase
+console.log(sentence.toLowerCase())
+
+//Q3
+//includes
+
+console.log(sentence.includes("coding"))
+
+
+//Q4
+// array
+
+let animals = ["cat","dog","elephant"]
+console.log(animals)
+
+//Q5
+//.push()
+animals.push("lion")
+console.log(animals)
+
+//.unshift()
+animals.unshift("fish")
+console.log(animals)
+
+
+//Q6
+//.pop()
+animals.pop()
+console.log(animals)
+
+//.switch()
+animals.shift()
+console.log(animals)
+
+//Q7
+//.includes()
+console.log(animals.includes("dog"))
+
+
+//.length()
+console.log(animals.length)
+
